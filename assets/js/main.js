19ec2d4d70e96583a4b7e2c0c73113da47b3501b
Basim <basim.abdullah247@gmail.com>
2021-01-16 13:34:46 +0500

    Intialize

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+/// Menu Toggle
+const toggleMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+toggleMenu('nav-toggle', 'nav-menu')
+
+/// Active and remove active
+const navLink = document.querySelectorAll('.navLink')
+
+function linkAction() {
+
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove menu on mobile after clicking nav link
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* Scroll Animations */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/* Scroll home */
+sr.reveal('.homeTitle', {})
+sr.reveal('.homeImg', {})
+sr.reveal('.homeSocialIcon', { delay: 200 })
+
+/* Scroll About*/
+sr.reveal('.aboutImg', {});
+sr.reveal('.aboutSubtitle', { delay: 400 });
+sr.reveal('.aboutText', { delay: 400 });
+
+/* Scroll Work */
+const sw = ScrollReveal({
+    origin: 'bottom',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+sw.reveal('.workImgFirst', { interval: 200 });
+sw.reveal('.workImg', { interval: 400 });
+sw.reveal('.workImgLast', { interval: 600 });
+
+/* Scroll work heading in view */
+gsap.registerPlugin(ScrollTrigger);
+//while (1)
+gsap.to(".workSubtitle", {
+    scrollTrigger: {
+        trigger: ".workSubtitle",
+        start: "top center",
+        end: "bottom 80px",
+        scrub: true,
+        pin: true,
+        toggleActions: "restart pause reverse pause"
+    },
+
+    duration: 3
+});
+
+/* Animate work images */
+gsap.to(".workImg", {
+    scrollTrigger: {
+        scrollTrigger: {
+            trigger: ".workImg",
+            start: "top center",
+            end: "bottom 80px",
+            scrub: true,
+            markers: true,
+            toggleActions: "restart pause reverse pause"
+        },
+        ease: 200,
+        duration: 3
+    }
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use("/assets/css", express.static(__dirname + "/assets/css"));
+app.use("/assets/img", express.static(__dirname + "/assets/img"));
+app.use("/assets/js", express.static(__dirname + "/assets/js"));
+
+app.get("/", function(req, res) {
+    console.log("Index Loaded");
+    res.sendFile(__dirname + "/index.html");
+});
+app.listen(port, function() {
+    console.log("Server is Live");
+})
