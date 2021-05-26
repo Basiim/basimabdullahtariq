940e5cef9680318e4f6ca75f7a6cac4429a4030f
Basim <basim.abdullah247@gmail.com>
2021-05-26 17:29:24 +0500

    ssl

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.get("/", function(req, res) {
     console.log("Index Loaded");
     res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/.well-known/pki-validation/47C4506955AE6D89F1CFEC9BB5211974.txt", function(req, res) {
+    res.sendFile(__dirname + "/.well-known/pki-validation/47C4506955AE6D89F1CFEC9BB5211974.txt")
+})
 app.listen(port, function() {
     console.log("Server is Live");
 })
